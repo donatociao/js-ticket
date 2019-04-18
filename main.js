c84877b845c59275f8d01282a07e1f8d339dc578
donatociao <ciao.donatociao@gmail.com>
2019-04-18 10:57:28 +0200

    optimized

--- main.js
+++ main.js
@@ -3,15 +3,15 @@ var anni = prompt('Quanti anni hai?'); //anni passeggero
 var prezzo = km * 0.21; //Tariffa 0.21 €/km
 
 if (anni < 18) { //se il passeggero è minorenne
-  var scontominori = prezzo / 1.20; //sconto 20%
+  var scontominori = prezzo * 0.20; //sconto 20%
   console.log('Il costo del biglietto intero è ' + prezzo + ' €');
-  console.log('La tariffa ridotta per i minorenni è di ' + scontominori + ' €');
+  console.log('La tariffa ridotta per i minorenni è di ' + (prezzo - scontominori) + ' €');
 }
 
 else if (anni > 65) { //se il passeggero è Over65
-  var scontoover = prezzo / 1.40; //sconto 40%
+  var scontoover = prezzo * 0.40; //sconto 40%
   console.log('Il costo del biglietto intero è di ' + prezzo + ' €');
-  console.log('La tariffa ridotta per gli over 65 è di ' + scontoover + ' €');
+  console.log('La tariffa ridotta per gli over 65 è di ' + (prezzo - scontoover) + ' €');
 }
 
 else {
